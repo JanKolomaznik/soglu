0853d67d425454893e643571a38a6254615007dc
Jan Kolomaznik <honza.kolomaznik@gmail.com>
2014-06-02 13:28:20 +0200

    Replacing boost smart pointers with std smart pointers.

--- include/soglu/CgFXShader.hpp
+++ include/soglu/CgFXShader.hpp
@@ -18,7 +18,7 @@
 #include <ostream>
 
 
-#include <boost/shared_ptr.hpp>
+#include <memory>
 #include <boost/filesystem.hpp>
 #include <boost/function.hpp>
 //#include <boost/graph/graph_concepts.hpp>
@@ -142,10 +142,10 @@ protected:
 };
 
 template< typename TResource >
-boost::shared_ptr< ResourceGuard< TResource > >
+std::shared_ptr< ResourceGuard< TResource > >
 makeResourceGuardPtr( boost::function< TResource() > aAcquisition, boost::function< void(TResource &) > aRelease, bool aAcquire = true )
 {
-	return boost::shared_ptr< ResourceGuard< TResource > >( new ResourceGuard< TResource >( aAcquisition, aRelease, aAcquire ) );
+	return std::shared_ptr< ResourceGuard< TResource > >( new ResourceGuard< TResource >( aAcquisition, aRelease, aAcquire ) );
 }
 
 
@@ -243,7 +243,7 @@ public:
 	{ return mEffectInitialized; }
 protected:
 
-	boost::shared_ptr< ResourceGuard< CGeffect > >	mCgEffect;
+	std::shared_ptr< ResourceGuard< CGeffect > >	mCgEffect;
 	std::map< std::string, CGtechnique >	mCgTechniques;
 	std::string	mEffectName;
 
